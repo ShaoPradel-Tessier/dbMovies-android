a4a9e80f20808559200894022e258f53f5c106e8
Shao <shao.p.tessier@gmail.com>
2020-02-02 22:27:10 -0500

    Adding the technical card in adding a movie

---
+++ app/src/main/java/ahuntsic/android/edu/bdfilms/AddActivity.java
@@ -0,0 +1,13 @@
+package ahuntsic.android.edu.bdfilms;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class AddActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/ahuntsic/android/edu/bdfilms/MainActivity.java
+++ app/src/main/java/ahuntsic/android/edu/bdfilms/MainActivity.java
@@ -9,9 +9,12 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SearchView;
 import android.widget.Toast;
 
@@ -20,6 +23,7 @@ import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity {
 
     Toolbar myToolBar;
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +33,13 @@ public class MainActivity extends AppCompatActivity {
         myToolBar = findViewById(R.id.act_toolbar1);
         setSupportActionBar(myToolBar);
 
-
+        btnAdd = findViewById(R.id.btnAddMain);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addMovie = new Intent(MainActivity.this, AddActivity.class);
+            }
+        });
     }
 
     @Override
@@ -48,13 +58,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextSubmit(String s) {
                 //When submit the string
-                Toast.makeText(MainActivity.this.getBaseContext(),"Recherche de film :" + s, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this.getBaseContext(), "Recherche de film :" + s, Toast.LENGTH_SHORT).show();
                 return true;
             }
 
             @Override
             public boolean onQueryTextChange(String s) {
-                //search on change or save the last string in search
+                //search on change or save the last string in searchinput
                 return false;
             }
         });
@@ -67,12 +77,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.tous_films:
-                Toast.makeText(this.getBaseContext(),"Tous les films", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this.getBaseContext(), "Tous les films", Toast.LENGTH_SHORT).show();
                 return true;
-                default:
-                    return super.onOptionsItemSelected(item);
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
